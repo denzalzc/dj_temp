fccf9220fbac214e6bfb4ce1ca89c24fd5f68709
Заболотный Денис <zabolotnyj@imc-dmn.parus-s.ru>
2026-02-05 13:56:47 +0400

    init

--- base_app/base_app/urls.py
+++ base_app/base_app/urls.py
@@ -16,17 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls.static import static
 from django.conf import settings
-from present.views import *
+from django.conf.urls.static import static
+from present.views import index, catch_all
 
 urlpatterns = [
     path('YaMaIkA/', admin.site.urls),
     path('', index, name='home'),
     path('<path:path>/', catch_all),
 ]
-
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
